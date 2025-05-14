e56aa877045730acfb2f1860f54e0cd222a57f9c
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2025-05-14 20:29:38 +0630

    auth

--- internal/handler/auth_handler.go
+++ internal/handler/auth_handler.go
@@ -1,6 +1,12 @@
 package handlers
 
-import "github.com/ecommerce-go/internal/service"
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/ecommerce-go/internal/service"
+)
 
 type AuthHandler struct{
 	auth services.AuthService
@@ -12,3 +18,50 @@ func NewAuthHandler(a services.AuthService) *AuthHandler {
 
 
 
+func (h *AuthHandler) Signup(w http.ResponseWriter, r *http.Request)  {
+	
+	 var req struct{
+			name string `json:"name"`
+			Password string `json:"password"`
+	 }
+
+	 if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	 }
+
+	 user, err := h.auth.Register(req.name, req.Password)
+
+	 if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+	w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(user)
+
+}
+
+
+func (h *AuthHandler) Login (w http.ResponseWriter , r *http.Request){
+
+	var req struct {
+        name string `name:"username"`
+        Password string `json:"password"`
+    }
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+	token, err := h.auth.Login(req.name, req.Password)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusUnauthorized)
+        return
+    }
+    json.NewEncoder(w).Encode(map[string]string{
+        "token": token,
+        "exp":   time.Now().Add(time.Hour * 24).Format(time.RFC3339),
+    })
+}
