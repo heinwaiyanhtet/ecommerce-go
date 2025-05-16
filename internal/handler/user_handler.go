63a962af2791ea97ec894d4a60aa78e0924bb051
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2025-05-16 19:56:16 +0630

    format change in users

--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -4,11 +4,14 @@ import (
 	"encoding/json"
 	"net/http"
 	"github.com/ecommerce-go/internal/service"
+	"github.com/ecommerce-go/internal/model"
 )
 
 type UserHandler struct {
 	service services.UserService
 }
+
+
 
 func NewUserHandler(service services.UserService) *UserHandler {
 	return &UserHandler{service: service}
@@ -29,16 +32,30 @@ func (h *UserHandler) GetUser(w http.ResponseWriter, r *http.Request) {
 }
 
 
+
+
 func (h *UserHandler) GetAllUsers(w http.ResponseWriter, r *http.Request) {
 
     users, err := h.service.GetAllUsers()
+
     if err != nil {
         http.Error(w, "failed to get users", http.StatusInternalServerError)
         return
     }
 
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(users)
+	w.Header().Set("Content-Type", "application/json")
 
+	userVals := make([]models.User, len(users))
+
+	for i, u := range users {
+		if u != nil {
+			userVals[i] = *u
+		}
+	}
+
+
+	response := models.UsersResponse{Value: userVals}
+	json.NewEncoder(w).Encode(response)
 }
+
 
