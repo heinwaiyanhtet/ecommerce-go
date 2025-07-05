e3a5f6ad7a444631d5ea748f74bc6112944adbbf
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2025-07-05 23:06:10 +0630

    main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -25,7 +25,16 @@ func main() {
 	fmt.Println("DB_HOST:", os.Getenv("DB_HOST"))
 	fmt.Println("DB_PORT:", os.Getenv("DB_PORT"))
 
-	// Build DSN and connect to DB
+
+
+	fmt.Println("DB_HOST:", os.Getenv("DB_HOST"))
+	fmt.Println("DB_PORT:", os.Getenv("DB_PORT"))
+	fmt.Println("DB_USER:", os.Getenv("DB_USER"))
+	fmt.Println("DB_PASSWORD:", os.Getenv("DB_PASSWORD"))
+	fmt.Println("DB_NAME:", os.Getenv("DB_NAME"))
+
+
+	// // Build DSN and connect to DB
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
@@ -34,7 +43,11 @@ func main() {
 		os.Getenv("DB_NAME"),
 	)
 
+
+	fmt.Println("DB_HOST:", dsn)
+
 	db, err := sql.Open("mysql", dsn)
+
 	if err != nil {
 		log.Fatalf("Error opening database: %v", err)
 	}
